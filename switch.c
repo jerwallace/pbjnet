b602e7675a759a865c15cbf466b839fccc117703
Bahman Razmpa <razmpa@gmail.com>
2013-07-22 20:14:44 -0700

    add switch skeleton code

---
+++ switch.c
@@ -0,0 +1,28 @@
+/*
+ * switch.c
+ *
+ *  Created on: Jul 22, 2013
+ *      Author: Bahman
+ */
+
+#include "switch.h";
+
+
+void switch_init() {
+
+}
+
+
+void switch_add_entry(ip_address_t *address, int port) {
+
+}
+
+
+void switch_thread_routine(void *args) {
+
+}
+
+
+void switch_free() {
+
+}
